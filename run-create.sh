639d24a5d4619b6b91c4d1ce4c2022dee97996f1
udhos <udhos>
2019-08-28 17:53:41 -0300

    Create instance.

--- run-create.sh
+++ run-create.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-aws cloudformation create-stack --stack-name demo-web --template-body file://template/web.yaml --parameters ParameterKey=VPCName,ParameterValue=vpc-demo-web
+die() {
+	echo >&2 $0: $@
+	exit 1
+}
+
+echo KEYNAME=[$KEYNAME]
+
+[ -n "$KEYNAME" ] || die missing KEYNAME
+
+aws cloudformation create-stack --stack-name demo-web --template-body file://template/web.yaml --parameters ParameterKey=VPCName,ParameterValue=vpc-demo-web ParameterKey=KeyName,ParameterValue=$KEYNAME
--- run-update.sh
+++ run-update.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-aws cloudformation update-stack --stack-name demo-web --template-body file://template/web.yaml
+die() {
+        echo >&2 $0: $@
+        exit 1
+}
+
+echo KEYNAME=[$KEYNAME]
+
+[ -n "$KEYNAME" ] || die missing KEYNAME
+
+aws cloudformation update-stack --stack-name demo-web --template-body file://template/web.yaml --parameters ParameterKey=VPCName,ParameterValue=vpc-demo-web ParameterKey=KeyName,ParameterValue=$KEYNAME
+
 
